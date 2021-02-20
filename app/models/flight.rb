6cb8a60a6d34d94af2974795487b56acaa31b2e9
Tommym0ntana <tommy.montana.harwood@gmail.com>
2021-02-20 16:00:07 +0100

    Add validations to passengers and flights

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -3,4 +3,23 @@ class Flight < ApplicationRecord
   has_many :seats, dependent: :destroy
   has_many :passengers, dependent: :nullify
   has_one :flight_execution
+
+  validates :airplain_id, presence: true
+  
+  validates :origin, :destination, :duration, presence: true
+
+  validates :time, presence: true
+  # greater that present 
+  validates :date, presence: true
+
+  validate :minimum_duration
+
+  private
+
+  def minimum_duration
+    if duration < 30 
+      errors.add(:duration, "can't be less than 30 min")
+    end
+  end
+
 end
--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -2,4 +2,10 @@ class Passenger < ApplicationRecord
   belongs_to :user
   belongs_to :flight
   has_many :seats, dependent: :nullify
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true
+  validates :full_name, length: { minimum: 5 }
+  validates :telephone_number, length: { minimum: 9, maximum: 10 }, format: { with: /\A\d+\Z/ }
+  validates_length_of :national_id, is: 16
+  validates :national_id, presence: true
+
 end
